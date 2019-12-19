ccebb5e72af7fff30a4568f7aac44f469b701f71
Ben <reidben24@gmail.com>
2019-12-19 01:26:29 -0500

    Reorganize main(). Add vscode files

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -6,10 +6,13 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    DebugOverlays::toggleOverlay(DebugOverlays::None);
+    
     Properties::PrimaryMenuFont.loadFromFile(Properties::FontPath+"Abea.ttf");
     Properties::SecondaryMenuFont.loadFromFile(Properties::FontPath+"Abea.ttf");
     Properties::ConversationFont.loadFromFile(Properties::FontPath+"Abea.ttf");
+
+    DebugOverlays::toggleOverlay(DebugOverlays::None);
     Game::get().run();
+
 	return 0;
 }
